204452dd23cf2055cddc464d507ff53100fd2380
sunnyshoun <chej40505@gmail.com>
2024-06-17 20:20:13 +0800

    add dotenv

--- localVals.py
+++ localVals.py
@@ -1,5 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+GUILD_ID = 447012513209516052
 SUDOKU_URL = "https://sudoku-online.com/"
 BOARD_DATA_PATH = "./board_data.pickle"
-BOT_TOKEN = "MTI1MDQzODYyNjQwMDE0NTQ4MA.GV3eAW.EhLpk9FGYXXomRqBGCg1bg14GrJQ6c9GMagHJM"
-GUILD_ID = 447012513209516052
 CORRECT_ANI_PATH = "./assets/ani/correct.gif"
